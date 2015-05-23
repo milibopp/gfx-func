3d0ff1b399cdb395c98fabbec7064ece11e7342f
Eduard Bopp <eduard.bopp@aepsil0n.de>
2015-05-23 21:28:40 +0200

    Adapt to upstream changes
    Batch is now object-safe. Thus the DynamicBatch workaround is no longer
    required.

--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,19 @@
 extern crate gfx;
 
-use gfx::{ Resources, ParamStorage };
-use gfx::device::handle::Program;
-use gfx::batch::{ Batch, BatchData };
-
-
-pub trait DynamicBatch<R: Resources> {
-    fn get_data(&self) -> Result<BatchData<R>, String>;
-    fn fill_params(&self, values: &mut ParamStorage<R>) -> Result<&Program<R>, String>;
-}
-
-impl<B: Batch<R>, R: Resources> DynamicBatch<R> for B {
-    fn get_data(&self) -> Result<BatchData<R>, String> {
-        Batch::get_data(self)
-            .map_err(|err| format!("{:?}", err))
-    }
-
-    fn fill_params(&self, values: &mut ParamStorage<R>) -> Result<&Program<R>, String> {
-        Batch::fill_params(self, values)
-            .map_err(|err| format!("{:?}", err))
-    }
-}
-
-impl<'a, R: Resources> Batch<R> for Box<DynamicBatch<R> + 'a> {
-    type Error = String;
-    fn get_data(&self) -> Result<BatchData<R>, String> {
-        DynamicBatch::get_data(&**self)
-    }
-
-    fn fill_params(&self, values: &mut ParamStorage<R>) -> Result<&Program<R>, String> {
-        DynamicBatch::fill_params(&**self, values)
-    }
-}
-
-impl<'a, R: Resources> Batch<R> for &'a DynamicBatch<R> {
-    type Error = String;
-    fn get_data(&self) -> Result<BatchData<R>, String> {
-        DynamicBatch::get_data(&**self)
-    }
-
-    fn fill_params(&self, values: &mut ParamStorage<R>) -> Result<&Program<R>, String> {
-        DynamicBatch::fill_params(&**self, values)
-    }
-}
+use gfx::Resources;
+use gfx::batch::Batch;
 
 
 pub enum Batches<'a, R> {
     Empty,
-    Single(&'a DynamicBatch<R>),
-    Iter(Box<Iterator<Item=&'a DynamicBatch<R>> + 'a>),
+    Single(&'a Batch<R>),
+    Iter(Box<Iterator<Item=&'a Batch<R>> + 'a>),
 }
 
 impl<'a, R> Iterator for Batches<'a, R> {
-    type Item = &'a DynamicBatch<R>;
+    type Item = &'a Batch<R>;
 
-    fn next(&mut self) -> Option<&'a DynamicBatch<R>> {
+    fn next(&mut self) -> Option<&'a Batch<R>> {
         use std::mem;
         use self::Batches::*;
         let mut tmp = Empty;
