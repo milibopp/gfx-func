1856496cfd2e21c00b2c6c14e41dbe8356907dd4
Eduard Bopp <eduard.bopp@aepsil0n.de>
2015-05-23 20:04:36 +0200

    Use shared, owned batch in a signal

--- examples/triangle.rs
+++ examples/triangle.rs
@@ -2,6 +2,7 @@
 extern crate gfx;
 extern crate gfx_device_gl;
 extern crate glutin;
+extern crate carboxyl;
 extern crate carboxyl_window;
 extern crate window;
 extern crate input;
@@ -11,13 +12,15 @@ extern crate gfx_func;
 
 use std::rc::Rc;
 use std::cell::RefCell;
+use std::sync::Arc;
+use carboxyl::Signal;
 use carboxyl_window::{ SourceWindow, EventSource };
 use window::{ WindowSettings };
 use shader_version::OpenGL;
 use glutin_window::GlutinWindow;
-use gfx::traits::{ FactoryExt, ToSlice };
+use gfx::traits::FactoryExt;
 use gfx::{ Stream, Resources, ClearData };
-use gfx::batch::Context;
+use gfx::batch::OwnedBatch;
 use gfx_func::{ Element };
 
 pub mod shared_win;
@@ -29,10 +32,10 @@ gfx_vertex!( Vertex {
 });
 
 fn run_from_source<R, W, E, F, S>(source: &mut SourceWindow<W>, stream: &mut S,
-                                  mut render: F, element: E)
+                                  mut render: F, element: Signal<Arc<E>>)
     where R: Resources,
           W: EventSource,
-          E: Element<R>,
+          E: Element<R> + 'static,
           S: Stream<R>,
           F: FnMut(&mut S),
 {
@@ -43,7 +46,8 @@ fn run_from_source<R, W, E, F, S>(source: &mut SourceWindow<W>, stream: &mut S,
             depth: 1.0,
             stencil: 0,
         });
-        for batch in element.batches() {
+        let current = element.sample();
+        for batch in current.batches() {
             stream.draw(&batch).unwrap();
         }
         render(stream)
@@ -58,16 +62,13 @@ fn main() {
     let (mut stream, mut device, mut factory) = shared_win::init_shared(window.clone());
     let mut source = SourceWindow::new(window.clone(), 10_000_000);
 
-    let mut context = Context::new();
-
-    let batch = {
+    let batch = Arc::new({
         let vertex_data = [
             Vertex { pos: [ -0.5, -0.5 ], color: [1.0, 0.0, 0.0] },
             Vertex { pos: [  0.5, -0.5 ], color: [0.0, 1.0, 0.0] },
             Vertex { pos: [  0.0,  0.5 ], color: [0.0, 0.0, 1.0] },
         ];
         let mesh = factory.create_mesh(&vertex_data);
-        let slice = mesh.to_slice(gfx::PrimitiveType::TriangleList);
         let program = {
             let vs = gfx::ShaderSource {
                 glsl_120: Some(include_bytes!("triangle_120.glslv")),
@@ -81,13 +82,12 @@ fn main() {
             };
             factory.link_program_source(vs, fs).unwrap()
         };
-        let state = gfx::DrawState::new();
-        context.make_batch(&program, None, &mesh, slice, &state).ok().unwrap()
-    };
+        OwnedBatch::new(mesh, program, None).unwrap()
+    });
 
     run_from_source(
         &mut source, &mut stream,
         |s| s.present(&mut device),
-        (&batch, &context),
+        Signal::new(batch)
     );
 }
