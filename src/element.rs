44145650a0ac9a41bfcf3e4021a3800d23e7e79c
Eduard Bopp <eduard.bopp@aepsil0n.de>
2015-05-24 12:16:20 +0200

    Add clear element and refactor wrap batch element

--- src/element.rs
+++ src/element.rs
@@ -1,7 +1,6 @@
 //! Drawable elements
 
-use gfx::Resources;
-use gfx::batch::Batch;
+use gfx::{ Resources, ClearData, batch };
 
 use command::{ Commands, Command };
 
@@ -12,8 +11,41 @@ pub trait Element<R: Resources> {
     fn commands(&self) -> Commands<R>;
 }
 
-impl<B: Batch<R>, R: Resources> Element<R> for B {
+
+/// A single batch.
+#[derive(Clone)]
+pub struct Batch<B>(pub B);
+
+impl<B: batch::Batch<R>, R: Resources> Element<R> for Batch<B> {
     fn commands(&self) -> Commands<R> {
-        Commands::Single(Command::Draw(self))
+        Commands::Single(Command::Draw(&self.0))
+    }
+}
+
+
+/// An element preceded by clearing the screen
+#[derive(Clone)]
+pub struct Cleared<E> {
+    clear: ClearData,
+    element: E,
+}
+
+impl<E> Cleared<E> {
+    pub fn new(clear: ClearData, element: E) -> Cleared<E> {
+        Cleared { clear: clear, element: element }
+    }
+}
+
+impl<E: Element<R>, R: Resources> Element<R> for Cleared<E>
+    where R::Buffer: 'static, R::ArrayBuffer: 'static, R::Shader: 'static,
+          R::Program: 'static, R::FrameBuffer: 'static, R::Surface: 'static,
+          R::Sampler: 'static, R::Texture: 'static, R: 'static,
+{
+    fn commands(&self) -> Commands<R> {
+        Commands::Iter(Box::new(
+            Some(Command::Clear(self.clear))
+                .into_iter()
+                .chain(self.element.commands())
+        ))
     }
 }
