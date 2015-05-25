81a75b8f0fde5840293f133802c8f0caad3f510d
Eduard Bopp <eduard.bopp@aepsil0n.de>
2015-05-25 15:33:44 +0200

    Import and adapt reactive camera

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
 //! High-level functional 3D graphics API on top of gfx-rs.
 
 extern crate gfx;
+extern crate num;
+extern crate nalgebra;
+#[macro_use(lift)]
+extern crate carboxyl;
+extern crate carboxyl_window;
 
 pub use element::Element;
 
 pub mod element;
 pub mod command;
+pub mod cam;
